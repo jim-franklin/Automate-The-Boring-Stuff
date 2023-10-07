c3bf9f900536f3c68e9b8149b1d4ac0dbd530779
jimFranklin <jimmyfranklin2000@gmail.com>
2023-10-07 20:50:29 +0000

    add input_validation.py

---
+++ Practice-Projects/Chapter-03/input_validation.py
@@ -0,0 +1,22 @@
+def collatz(integer):
+    # Check if even
+    if integer % 2 == 0:
+        return integer // 2
+    # Check if odd
+    elif integer % 2 == 1:
+        return 3 * integer + 1
+
+
+print('Enter number:')
+try:
+    number = input()    # ask user for input
+    number = int(number)    # convert user input (sting value) to integer
+    
+    # the while loop breaks out when number = 1
+    while number > 1:
+        # the collatz function is called and its return value assigned to the variable, number
+        number = collatz(number)    
+        print(number) 
+
+except ValueError:
+    print('Error: You must Enter an integer.')
