ac85e1d73a159b50342bf95cc3c33488af868fab
jimFranklin <jimmyfranklin2000@gmail.com>
2023-10-07 23:27:09 +0000

    update input_validation.py

--- Practice-Projects/Chapter-03/input_validation.py
+++ Practice-Projects/Chapter-03/input_validation.py
@@ -1,5 +1,6 @@
-# Add try and except statements to the previous project to detect whether 
+# Add try and except statements to the previous project to detect whether
 # the user types in a noninteger string
+
 
 def collatz(integer):
     # Check if even
@@ -10,17 +11,17 @@ def collatz(integer):
         return 3 * integer + 1
 
 
-print('Enter number:')
+print("Enter number:")
 try:
-    number = input()    # ask user for input
-    number = int(number)    # convert user input (sting value) to integer
-    
+    number = input()  # ask user for input
+    number = int(number)  # convert user input (sting value) to integer
+
     # the while loop breaks out when number = 1
     while number > 1:
-        # the collatz function is called and its return value assigned to 
+        # the collatz function is called and its return value assigned to
         # the variable, number
-        number = collatz(number)    
-        print(number) 
+        number = collatz(number)
+        print(number)
 
 except ValueError:
-    print('Error: You must Enter an integer.')
+    print("Error: You must Enter an integer.")
