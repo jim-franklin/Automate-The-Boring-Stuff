60f4dbfc2b21463ffa4df75cbda6c2eabda76ab2
jimFranklin <jimmyfranklin2000@gmail.com>
2023-10-07 20:51:08 +0000

    update the_collatz_sequence.py

--- Practice-Projects/Chapter-03/the_collatz_sequence.py
+++ Practice-Projects/Chapter-03/the_collatz_sequence.py
@@ -8,15 +8,12 @@ def collatz(integer):
 
 
 print('Enter number:')
-try:
-    number = input()    # ask user for input
-    number = int(number)    # convert user input (sting value) to integer
-    
-    # the while loop breaks out when number = 1
-    while number > 1:
-        # the collatz function is called and its return value assigned to the variable, number
-        number = collatz(number)    
-        print(number) 
+number = input()    # ask user for input
+number = int(number)    # convert user input (sting value) to integer
 
-except ValueError:
-    print('Error: You must Enter an integer.')
+# the while loop breaks out when number = 1
+while number > 1:
+    # the collatz function is called and its return value assigned to the variable, number
+    number = collatz(number)    
+    print(number)
+    
