fbf6b8b065ada7083f6e2d7e481d42ed19c7941e
Jim Franklin <jimmyfranklin2000@gmail.com>
2023-10-22 23:26:22 +0000

    update inventory.py

--- Practice-Projects/chapter-05/inventory.py
+++ Practice-Projects/chapter-05/inventory.py
@@ -2,7 +2,7 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
 
 
 def display_inventory(inventory):
-    print("Inventory:")
+    print('Inventory:')
     
     item_total = 0  # set total number of items to 0
     
@@ -10,7 +10,7 @@ def display_inventory(inventory):
         print(str(v) + ' ' + k)
         item_total += v
         
-    print("Total number of items: " + str(item_total))
+    print('Total number of items: ' + str(item_total))
 
 
 # Call the function with `stuff` as argument
