2b4a54b2444f425859f64d50d0e29786995adece
Jim Franklin <jimmyfranklin2000@gmail.com>
2023-10-23 00:35:59 +0000

    included Chapter-5

---
+++ Practice-Projects/Chapter-05/add_to_inventory.py
@@ -0,0 +1,21 @@
+def addToInventory(inventory, addedItems):
+    
+    for item in addedItems:
+        inventory.setdefault(item, 0)   # set the value of a non-exitent key to 0
+        # add 1 to the value of an item in the inventory if item can be found in `addedItems` list
+        inventory[item] += 1    
+                                
+    item_total = 0  # set total number of items to 0
+    
+    for k, v in inventory.items():
+        print(str(v), k)
+        item_total += v
+        
+    print(f'Total number of items: {item_total}')
+
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+# Call the function with `inv` and `dragonLoot` as arguments
+addToInventory(inv, dragonLoot)
---
+++ Practice-Projects/Chapter-05/inventory.py
@@ -0,0 +1,17 @@
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+
+def display_inventory(inventory):
+    print('Inventory:')
+    
+    item_total = 0  # set total number of items to 0
+    
+    for k, v in inventory.items():
+        print(str(v) + ' ' + k)
+        item_total += v
+        
+    print('Total number of items: ' + str(item_total))
+
+
+# Call the function with `stuff` as argument
+display_inventory(stuff)
